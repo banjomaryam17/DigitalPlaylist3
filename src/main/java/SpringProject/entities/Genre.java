7caf16b70591abde526cbee034f0006ade7713ea
maryam banjo <mitechnigeria@gmail.com>
2025-12-22 15:11:24 +0000

    Refactor: Added Comparables and deepEquals

--- src/main/java/SpringProject/entities/Genre.java
+++ src/main/java/SpringProject/entities/Genre.java
@@ -2,13 +2,15 @@ package SpringProject.entities;
 import lombok.*;
 import jakarta.validation.constraints.*;
 
+import java.util.Objects;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 
-public class Genre {
+public class Genre implements Comparable<Genre> {
 //    id,name,description
 
     @NonNull
@@ -21,5 +23,22 @@ public class Genre {
     @NonNull
     @NotBlank(message = "Genre description is required")
     private String description;
+
+    public static boolean deepEquals(Genre g1, Genre g2) {
+        return Objects.equals(g1.id, g2.id)
+                && Objects.equals(g1.name, g2.name)
+                && Objects.equals(g1.description, g2.description);
+    }
+
+    @Override
+    public int compareTo(Genre g) {
+        if (id < g.id) {
+            return -1;
+        } else if (id > g.id) {
+            return 1;
+        }
+        return 0;
+    }
+}
 
 }
